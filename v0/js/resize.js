c0cfd39a393605d2bba61718d43429e5db6eeed8
frosDfurret <64030476+frosDfurret@users.noreply.github.com>
2021-12-20 06:54:01 +0000

    anothjer updat

--- v0/js/resize.js
+++ v0/js/resize.js
@@ -1,7 +1,16 @@
-/*
 const time = document.getElementById('time')
-const periodContainer = document.getElementById('period-container')
+const period = document.getElementsByClassName('period')
 
-time.style.fontSize = window.innerWidth / 7 + window.innerHeight / 20 + "px"
-periodContainer.style.fontSize = window.innerWidth / 35 + window.innerHeight / 35 + "px"
-*/
+setInterval(function(){
+if(window.innerWidth < 500) {
+  time.style.fontSize = "17vw"
+  for (var i = 0; i < period.length; i++) {
+    period[i].fontSize = "4vw"
+  }
+} else {
+  time.style.fontSize = "15vh"
+  for (var i = 0; i < period.length; i++) {
+    period[i].fontSize = "4vh"
+  }
+}
+},10)
